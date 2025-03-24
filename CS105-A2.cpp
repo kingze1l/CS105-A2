84791f41ee297e37dab2b2acfc1d73633d87982a
270279231 <270279231@CIT3-205-13>
2025-03-24 15:16:53 +1300

    added course class with display method

--- CS105-A2.cpp
+++ CS105-A2.cpp
@@ -2,19 +2,68 @@
 //
 
 #include <iostream>
+#include<string>
+using namespace std;
+
+//student class  creation and definition
+class Student {
+private:
+	int rollno; //private attribute to encupsulate the data
+	string name;
+	string address;
+	string contact;
+
+public:
+	Student(int roll, string n, string addr, string cnt) : rollno(roll), name(n), address(addr), contact(cnt) {} //constructor to initialize the data;
+
+	//getter and setter methods to access the private data 
+	int getRollno() {return rollno;}
+	string getName() { return name; }
+	string getAddress() { return address; }
+	string getContact() { return contact; }
+
+	//display student details method
+	void display() {
+		cout << "Roll No: " << rollno << endl;
+		cout << "Name: " << name << endl;
+		cout << "Address: " << address << endl;
+		cout << "Contact: " << contact << endl;
+	}
+};
+
+class Course {
+private:
+	string courseName;
+	string courseCode;
+	string description;
+	string teacher;
+
+public:
+	Course(string cName, string cCode, string desc, string t) : courseName(cName), courseCode(cCode), description(desc), teacher(t) {} //constructor to initialize the data;
+	//getter and setter methods to access the private data 
+	string getCourseName() { return courseName; }
+	string getCourseCode() { return courseCode; }
+	string getDescription() { return description; }
+	string getTeacher() { return teacher; }
+	//display course details method
+	void display() {
+		cout << "Course Name: " << courseName << endl;
+		cout << "Course Code: " << courseCode << endl;
+		cout << "Description: " << description << endl;
+		cout << "Teacher: " << teacher << endl;
+	}
+};
+
+
 
 int main()
 {
+
+	Student student1(101, "Sami jan", "Kabul", "0781234567"); //creating object of student class
+	student1.display();
+	//test course 
+	Course math("Math", "M101", "Mathematics", "Mr. John");
+	math.display();
     std::cout << "Hello World!\n";
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
